57a7fb0ab6246aa9046317e42df54522a1577132
Nathan Grunzweig <nathan@codefresh.io>
2015-03-15 10:36:26 +0200

    the flow is working in general, need to make cf-api cli support tag variable

--- build.sh
+++ build.sh
@@ -5,34 +5,35 @@ TAG=$1
 
 [ -z "$TAG" ] && echo tag argument is required && exit 1
 
-ORIGINAL_DIR="$PWD"
-
-TMP_CF_API_PATH=/tmp/cf-api-clone
-
-rm -rf $TMP_CF_API_PATH || true
-git clone git@github.com:codefresh/cf-api.git $TMP_CF_API_PATH
-cd $TMP_CF_API_PATH
-git checkout $TAG
-
-cd $ORIGINAL_DIR
-
-CF_API_TARGET_DIR=/opt/codefresh/cf/cf-api
 CF_BASE_IMAGES_TARGET_DIR=/opt/codefresh/cf/cf-base-images
+CF_BASE_IMAGES_SOURCE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+CF_API_SOURCE_DIR=/tmp/cf-api-tmp
+CF_API_TARGET_DIR=/opt/codefresh/cf/cf-api
 
 MOUNT_DOCKER="-v /var/run/docker.sock:/var/run/docker.sock"
-MOUNT_CF_API="-e CF_API=$CF_API_TARGET_DIR -v $TMP_CF_API_PATH:$CF_API_TARGET_DIR"
-MOUNT_IMAGES="-e CF_BASE_IMAGES=$CF_BASE_IMAGES_TARGET_DIR -v $ORIGINAL_DIR:$CF_BASE_IMAGES_TARGET_DIR"
+MOUNT_IMAGES="-e CF_BASE_IMAGES=$CF_BASE_IMAGES_TARGET_DIR -v $CF_BASE_IMAGES_SOURCE_DIR:$CF_BASE_IMAGES_TARGET_DIR"
+MOUNT_CF_API="-e CF_API=$CF_API_TARGET_DIR -v $CF_API_SOURCE_DIR:$CF_API_TARGET_DIR"
 PASS_TAG="-e TAG=$TAG"
-CMD="bash $CF_BASE_IMAGES_TARGET_DIR/build/start-in-docker"
+CMD="bash -x $CF_BASE_IMAGES_TARGET_DIR/build/start-in-docker"
+MOUNT_USERS="-v /etc/passwd:/etc/passwd"
 
-IMAGE="codefresh/cf-base-images-builder:$TAG"
+IMAGE="codefresh/cf-base-images-builder"
+TAGGED_IMAGE="$IMAGE:$TAG"
 
-docker build -t $IMAGE ./build
+[ -e "$CF_API_SOURCE_DIR" ] && rm -rf $CF_API_SOURCE_DIR
+git clone git@github.com:codefresh/cf-api.git $CF_API_SOURCE_DIR
+cd $CF_API_SOURCE_DIR
+git checkout $TAG
+
+docker build -t $TAGGED_IMAGE $CF_BASE_IMAGES_SOURCE_DIR/build
 
 docker run \
+  --rm -it \
+  $MOUNT_USERS \
   $MOUNT_DOCKER \
   $MOUNT_CF_API \
   $MOUNT_IMAGES \
   $PASS_TAG \
-  $IMAGE
+  $TAGGED_IMAGE \
   $CMD
