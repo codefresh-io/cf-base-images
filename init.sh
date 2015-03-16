ff0942f93b0fbd6c2fd027b3b259ef60b1623118
Nathan Grunzweig <nathan@codefresh.io>
2015-03-16 18:23:20 +0200

    generate the Dockerfile of cf-base-ide out of a template with the correct tag of cf-base

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/usr/local/bin/bash
+set -e
+
+TAG=$1
+
+[ -z "$TAG" ] && echo TAG environment variable is required && exit 1
+
+sed 's/$TAG/develop/g' < images/base-ide/build/Dockerfile.template > images/base-ide/build/Dockerfile
+
