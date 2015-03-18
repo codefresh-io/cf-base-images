c030ae971fee692aa7883b9cba3f790bf6ce6c36
Nathan Grunzweig <nathan@codefresh.io>
2015-03-18 13:37:10 +0200

    bugfix, don't replace tag with constant develop, but with actual TAG input variable value

--- init.sh
+++ init.sh
@@ -5,5 +5,5 @@ TAG=$1
 
 [ -z "$TAG" ] && echo TAG environment variable is required && exit 1
 
-sed 's/$TAG/develop/g' < images/base-ide/build/Dockerfile.template > images/base-ide/build/Dockerfile
+sed "s/\$TAG/$TAG/g" < images/base-ide/build/Dockerfile.template > images/base-ide/build/Dockerfile
 
