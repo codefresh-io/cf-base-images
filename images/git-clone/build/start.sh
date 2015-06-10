a9807d143f0e5edd1e7f59d3c3f3c08b285f49f0
Nathan Grunzweig <nathan@codefresh.io>
2015-06-10 17:36:31 +0300

    add support for ssh, secrete key, ssh and consequently bit-bucket

--- images/git-clone/build/start.sh
+++ images/git-clone/build/start.sh
@@ -2,10 +2,14 @@ set -e
 
 echo on
 cd /
+
+echo "$PRIVATE_KEY" > /root/.ssh/codefresh
+chmod 700 ~/.ssh/
+chmod 600 ~/.ssh/*
+
 #eval "ssh-agent -s"
 #ssh-add $key
-echo "status is.. $status"
-echo "cloning ... $repo"
+echo "cloning  $repo"
 git clone $repo /src
 ls -la
 cd /src
