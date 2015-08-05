d95854f9814ce62f9570966ca3c53aadd0395f8e
Itai Gendler <itai@codefresh.io>
2015-08-05 15:43:49 +0300

    support tagging built images

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 set -e
 
 TAG=$1
+MODULE=$2
 
 [ -z "$TAG" ] && echo TAG environment variable is required && exit 1
 
@@ -16,3 +17,33 @@ sed \
   -e "s/\$UID/$RANDOM/g" \
    < images/base-ide/build/Dockerfile.template > images/base-ide/build/Dockerfile
 
+
+
+[ -e "images/base/build/Dockerfile.build" ] && rm images/base/build/Dockerfile.build
+cp images/base/build/Dockerfile images/base/build/Dockerfile.build
+cat >> images/base/build/Dockerfile.build << EOP
+LABEL io.codefresh.repo.owner=codefresh-io \\
+           io.codefresh.repo.name=$MODULE \\
+           io.codefresh.repo.branch=$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match) \\
+           io.codefresh.repo.sha=$(git rev-parse HEAD)
+EOP
+
+
+[ -e "images/base-ide/build/Dockerfile.build" ] && rm images/base-ide/build/Dockerfile.build
+cp images/base-ide/build/Dockerfile images/base-ide/build/Dockerfile.build
+cat >> images/base-ide/build/Dockerfile.build << EOP
+LABEL io.codefresh.repo.owner=codefresh-io \\
+           io.codefresh.repo.name=$MODULE \\
+           io.codefresh.repo.branch=$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match) \\
+           io.codefresh.repo.sha=$(git rev-parse HEAD)
+EOP
+
+
+[ -e "images/git-clone/build/Dockerfile.build" ] && rm images/git-clone/build/Dockerfile.build
+cp images/git-clone/build/Dockerfile images/git-clone/build/Dockerfile.build
+cat >> images/git-clone/build/Dockerfile.build << EOP
+LABEL io.codefresh.repo.owner=codefresh-io \\
+           io.codefresh.repo.name=$MODULE \\
+           io.codefresh.repo.branch=$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match) \\
+           io.codefresh.repo.sha=$(git rev-parse HEAD)
+EOP
